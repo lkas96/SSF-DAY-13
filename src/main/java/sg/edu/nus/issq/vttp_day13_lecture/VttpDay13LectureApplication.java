938ff1316aea2e6ad2ec6c63d87e8c9fe8b6693d
Lawson Kwek <63045323+lkas96@users.noreply.github.com>
2024-11-20 10:48:39 +0800

    Springboot Initializer

---
+++ src/main/java/sg/edu/nus/issq/vttp_day13_lecture/VttpDay13LectureApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.issq.vttp_day13_lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VttpDay13LectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VttpDay13LectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/issq/vttp_day13_lecture/VttpDay13LectureApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.issq.vttp_day13_lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VttpDay13LectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
